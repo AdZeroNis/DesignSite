de720bcd700b04187f6848e87c5c868fe3e13b48
shghygh001 <shghyghkrimi2923@gmail.com>
2023-12-25 09:15:18 +0330

    Fix The Bug

--- mainPage.js
+++ mainPage.js
@@ -1,15 +1,17 @@
 window.addEventListener("DOMContentLoaded", function() {
-    var storedUsername = localStorage.getItem("username");
-    var storedPassword = localStorage.getItem("password");
-    if(storedPassword==null && storedUsername==null){
-      window.location.href="HTML2.html";
-    }
+  var storedUsername = localStorage.getItem("username");
+  var storedPassword = localStorage.getItem("password");
+  if(storedPassword==null && storedUsername==null){
+    window.location.href="HTML2.html";
+  }
 
-    if (storedUsername && storedPassword) {
-        var usernameInput = document.querySelector("#login .input-box input[type='text']");
-        var passwordInput = document.querySelector("#login .input-box input[type='password']");
+  if (storedUsername && storedPassword) {
+      var usernameInput = document.querySelector("#login .input-box input[type='text']");
+      var passwordInput = document.querySelector("#login .input-box input[type='password']");
 
-        usernameInput.value = storedUsername;
-        passwordInput.value = storedPassword;
-    }
+      if (usernameInput && passwordInput) {
+          usernameInput.value = storedUsername;
+          passwordInput.value = storedPassword;
+      }
+  }
 });
