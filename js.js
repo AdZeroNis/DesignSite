94266faa91cc8a934426259ad52a54eb46555321
shghygh001 <shghyghkrimi2923@gmail.com>
2023-12-25 09:23:39 +0330

    Edit Function

--- js.js
+++ js.js
@@ -100,12 +100,12 @@ function validateLoginForm() {
 
 var a = document.getElementById("loginBtn");
 var b = document.getElementById("registerBtn");
-var x = document.getElementById("login");
-var y = document.getElementById("register");
 
 
 
 function login() {
+    var x = document.getElementById("login");
+    var y = document.getElementById("register");
     x.style.left = "4px";
     y.style.right = "-520px";
     a.className += " white-btn";
